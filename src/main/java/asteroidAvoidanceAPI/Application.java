6e9c15eb6c614892d71039a24ab7c189361dfbe6
vkalavakuru <vkalavakuru@gmail.com>
2018-02-06 12:44:44 -0800

    second commit

--- src/main/java/asteroidAvoidanceAPI/Application.java
+++ src/main/java/asteroidAvoidanceAPI/Application.java
@@ -2,6 +2,9 @@ package asteroidAvoidanceAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +12,11 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+    
+    
 }
--- src/main/java/asteroidAvoidanceAPI/SafestLaunchDateController.java
+++ src/main/java/asteroidAvoidanceAPI/SafestLaunchDateController.java
@@ -1,21 +1,49 @@
 package asteroidAvoidanceAPI;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SafestLaunchDateController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/safestLaunchDate")
     public AsteroidAvoidance SafestLaunchDate(@RequestParam(value="name", defaultValue="Working on getting safest Launch date") String name) {
        
+    	
     	//TODO: Need to call NASA service and get NasaApiResult
-    	//TODO: Determine SafeLaunchDate and required data to populate as API result
+    	
+    	String neowsUrl = "https://api.nasa.gov/neo/rest/v1/feed";
+        String apiKey   ="H7V98iNStSoR1hRBQJgIMHWee0W6Pf5qvfyVLk4X";
+    	String startDate ="2015-09-07";
+    	String endDate ="2015-09-08";
+    	String ampersand ="&";
+        String urlConstruct = new StringBuffer(neowsUrl).
+        						  append("?").
+                                  append("start_date=").append(startDate).
+                                  append(ampersand).
+                                  append("end_date=").append(endDate).
+                                  append(ampersand).
+                                  append("detailed=true").
+                                  append(ampersand).
+                                  append("api_key=").
+                                  append(apiKey).
+                                  toString();
+    	
+    	NasaApiResult nasaApiResult = restTemplate.getForObject(urlConstruct, NasaApiResult.class);
+    	
+    	//TODO: Determine safe Launch Date depending on the conditions or requirements  
+    	
     	
     	return new AsteroidAvoidance(counter.incrementAndGet(),
                             String.format(template, name));
